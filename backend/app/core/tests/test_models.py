955a38e30ee8fc36acd47d2c4bd7281715ff8e93
Benedict Olebu <cgolebu.dev@gmail.com>
2023-07-09 11:26:15 -0700

    modified tests_models.py

--- backend/app/core/tests/test_models.py
+++ backend/app/core/tests/test_models.py
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password)
+        self.assertTrue(user.check_password(password))
 
     def test_create_superuser(self):
         """Test creating superuser"""
