ca4a503f910dc064168d9c87865902fdf5a823ea
Benedict Olebu <cgolebu.dev@gmail.com>
2023-07-09 11:25:40 -0700

    modified test_models.py

--- backend/app/core/tests/test_models.py
+++ backend/app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -17,8 +18,7 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))
-
+        self.assertTrue(user.check_password(password)
 
     def test_create_superuser(self):
         """Test creating superuser"""
