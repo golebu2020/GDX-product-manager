da7ca7131540215eabc34a8b36f61faec5177613
Benedict Olebu <benedictolebu@gmail.com>
2023-09-04 16:15:56 -0700

    modifies script.groovy

--- script.groovy
+++ script.groovy
@@ -1,18 +1,18 @@
 #!/usr/bin/env groovy
 
 def testBuild(){
-    sh "docker-compose run app sh -c 'python manage.py wait_for_db && python manage.py test' "
     this.increment()
 }
 
 def increment(){
-    def jenkins_workspace = readFile("env.WORSPACE/version.xml")
+    def jenkins_workspace = readFile("${env.WORSPACE}/version.xml")
     matcher = jenkins_workspace.split(".")
     major = matcher[0]
     minor = matcher[1]
     patch = matcher[2]
 
     echo "Major: ${major}, Minor: ${minor}, Patch: ${patch}"
+    sh "bash ./test_build ${major}.${minor}.${patch}"
 }
 
 def pushImage(){
