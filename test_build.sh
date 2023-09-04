8bb14339376e0954aa9c4a4795080967cc1ae68c
Benedict Olebu <benedictolebu@gmail.com>
2023-09-04 16:56:08 -0700

    modified test_build.sh

--- test_build.sh
+++ test_build.sh
@@ -2,5 +2,6 @@
 
 export TAG=$1
 
+
+docker-compose run app sh -c 'python manage.py wait_for_db && python manage.py test'
 docker-compose build
-docker-compose run app sh -c 'python manage.py wait_for_db && python manage.py test' 
