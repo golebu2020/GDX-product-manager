6e7b0bfc1587a12653c65b7cfc72082a2538ffe0
Benedict Olebu <benedictolebu@gmail.com>
2023-09-04 17:35:35 -0700

    modifies docker-compose

--- test_build.sh
+++ test_build.sh
@@ -2,5 +2,5 @@
 
 export TAG=$1
 
-docker-compose run product_manager_app sh -c 'python manage.py wait_for_db && python manage.py test'
+docker-compose run pmapp sh -c 'python manage.py wait_for_db && python manage.py test'
 docker-compose build
